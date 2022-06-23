76872a99079ad4a0a827382c783a2d2ff2fd9dbe
adamapplegate <applegateadaml@gmail.com>
2022-06-22 22:27:18 -0600

    Added TODOs, minor changes

--- initialize-windows.ps1
+++ initialize-windows.ps1
@@ -9,31 +9,35 @@
 
 # Prompt for user input
 $Drive = Read-Host -Prompt 'What drive do you want to intiailize?'
-echo ""
+Write-Output ""
 
 # Initialize Directories
-echo "Initializing directories in ${drive}: ..."
+Write-Output "Initializing directories in ${drive}: ..."
 New-Item -ItemType Directory -Force -Path ${Drive}:\Code\Personal | Out-Null
 New-Item -ItemType Directory -Force -Path ${Drive}:\Code\Sandbox | Out-Null
 New-Item -ItemType Directory -Force -Path ${Drive}:\Code\Work | Out-Null
 New-Item -ItemType Directory -Force -Path ${Drive}:\Code\Misc | Out-Null
 New-Item -ItemType Directory -Force -Path ${Drive}:\Tools | Out-Null
 
-New-Item -ItemType Directory -Force -Path "$env:LOCALAPPDATA\nvim"
-echo "Directories intialized."
-echo ""
+New-Item -ItemType Directory -Force -Path "$env:LOCALAPPDATA\nvim" | Out-Null
+Write-Output "Directories intialized."
+Write-Output ""
 
 # Pin Directories to File Explorer QuickAccess
-echo "Pinning to Quick Access..."
+Write-Output "Pinning to Quick Access..."
 $o = new-object -com shell.application
 $o.Namespace("$env:USERPROFILE").Self.InvokeVerb("pintohome") #pin user directory
 $o.Namespace("${Drive}:\Code").Self.InvokeVerb("pintohome")
 $o.Namespace("${Drive}:\Tools").Self.InvokeVerb("pintohome")
-echo "Folders pinned. Don't forget to unpin folders you don't want from File Explorer Quick Access."
-echo ""
+# TODO:
+# -unpin folders
+# -make hidden files visible
+# -view file extensions
+Write-Output "Folders pinned. Don't forget to unpin folders you don't want from File Explorer Quick Access."
+Write-Output ""
 
 # Config Files
-echo "Installing config files..."
+Write-Output "Installing config files..."
 
 # Install vim-plug
 Invoke-WebRequest -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
@@ -41,13 +45,15 @@ Invoke-WebRequest -useb https://raw.githubusercontent.com/junegunn/vim-plug/mast
 # Copy-Item .\init.vim -Destination ~\.config\nvim
 Copy-Item .\init.vim -Destination ~\AppData\Local\nvim
 Copy-Item .\.gitconfig -Destination ~
-echo "Config files installed."
-echo ""
+# TODO copy qmk files to appropriate place
+# TODO     -windows terminal colors, default settings
+Write-Output "Config files installed."
+Write-Output ""
 
-echo "***TODO:***"
-echo "File Explorer:"
-echo "  *Unpin folders you don't want on quick access"
-echo "  *Enable 'View Hidden Files'"
-echo "  *Enable 'View file extensions'"
-echo "Install Programs:"
-echo "  *Neovim"
+Write-Output "***TODO:***"
+Write-Output "File Explorer:"
+Write-Output "  *Unpin folders you don't want on quick access"
+Write-Output "  *Enable 'View Hidden Files'"
+Write-Output "  *Enable 'View file extensions'"
+Write-Output "Install Programs:"
+Write-Output "  *Neovim"

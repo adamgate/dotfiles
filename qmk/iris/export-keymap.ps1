d9acf330118f4b5362435061e1fb0ec0813b2bd5
adamapplegate <applegateadaml@gmail.com>
2022-08-09 21:27:24 -0600

    Edited Iris export keymap script

--- qmk/iris/export-keymap.ps1
+++ qmk/iris/export-keymap.ps1
@@ -1,5 +1,6 @@
 # copy all items in the folder to the destination
 Copy-Item * -Destination "$env:USERPROFILE\qmk_firmware\keyboards\keebio\iris\keymaps\AdamApplegate" -Include *.h,*.c,*.mk
+Copy-Item * -Destination "..\..\..\qmk_firmware\keyboards\keebio\iris\keymaps\AdamApplegate" -Include *.h,*.c,*.mk
 
 # delete the powershell script that was moved over
 # Remove-Item -Path C:\Users\Adam\qmk_firmware\keyboards\keebio\iris\keymaps\AdamApplegate -Include *.ps1

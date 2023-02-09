bcf0e939501df7ffdf7f663d877ad84a5417f5b4
adamgate <applegateadaml@gmail.com>
2023-02-09 10:12:39 -0700

    Altered export scripts to create folder structure if it doesn't exist

--- qmk/iris/export-keymap.ps1
+++ qmk/iris/export-keymap.ps1
@@ -1,3 +1,7 @@
+# Create the folder structure if it doesn't exist
+New-Item -ItemType Directory -Force -Path "$env:USERPROFILE\qmk_firmware\keyboards\keebio\iris\keymaps\AdamApplegate"
+
+
 # copy the keymap files in the folder to the destination
 Copy-Item * -Destination "$env:USERPROFILE\qmk_firmware\keyboards\keebio\iris\keymaps\AdamApplegate" -Include *.h,*.c,*.mk
 Copy-Item * -Destination "..\..\..\qmk_firmware\keyboards\keebio\iris\keymaps\AdamApplegate" -Include *.h,*.c,*.mk
--- qmk/qk65/export-keymap.ps1
+++ qmk/qk65/export-keymap.ps1
@@ -1,4 +1,7 @@
+# Create the folder structure if it doesn't exist
+New-Item -ItemType Directory -Force -Path "$env:USERPROFILE\qmk_firmware\keyboards\qwertykeys\qk65\hotswap\keymaps\AdamApplegate"
+
 # copy all items in the folder to the destination
-Copy-Item * -Destination "$env:USERPROFILE\qmk_firmware\keyboards\qwertykeys\qk65\keymaps\AdamApplegate" -Include *.h,*.c,*.mk
+Copy-Item * -Destination "$env:USERPROFILE\qmk_firmware\keyboards\qwertykeys\qk65\hotswap\keymaps\AdamApplegate" -Include *.h,*.c,*.mk
 
 Write-Host "Successfully exported keymap." -ForegroundColor Green

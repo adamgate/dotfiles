00600d28034dee46c6753a58e0f2f272917e0c35
adamgate <applegateadaml@gmail.com>
2023-06-05 20:05:19 -0600

    added startpage url encoding

--- startpages/first-startpage/assets/js/script.js
+++ startpages/first-startpage/assets/js/script.js
@@ -3,7 +3,7 @@ function search(event) {
     if (x == "Enter") {
         event.preventDefault()
         let text = document.getElementById("searchbar").value;
-        url = 'https://www.google.com/?q=' + text
+        const url = `https://www.google.com/search?q=${encodeURI(text)}`
         window.open(url, '_self');
     }
 }

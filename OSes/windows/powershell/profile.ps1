ade17e2f5fb3c10c9c52986dbbfbecfa58bf1f0c
adamgate <applegateadaml@gmail.com>
2023-06-14 08:42:55 -0600

    added powershell modules

--- OSes/windows/powershell/profile.ps1
+++ OSes/windows/powershell/profile.ps1
@@ -1,3 +1,5 @@
 # Initialize Oh My Posh
 #oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/gruvbox-simple.omp.json" | Invoke-Expression
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/everforest.omp.json" | Invoke-Expression
+
+Import-Module Utils

ed3f83d5a9639d025c02fab13b72bb2b108d4e4f
adamgate <applegateadaml@gmail.com>
2024-11-21 18:49:46 -0700

    improve date format

--- startpages/.startpage/assets/js/script.js
+++ startpages/.startpage/assets/js/script.js
@@ -1,31 +1,39 @@
 function search(event) {
-    var x = event.key;
-    if (x == "Enter") {
-        event.preventDefault()
-        let text = document.getElementById("searchbar").value;
-        const url = `https://www.google.com/search?q=${encodeURIComponent(text)}`
-        window.open(url, '_self');
-    }
+  var x = event.key;
+  if (x == "Enter") {
+    event.preventDefault();
+    let text = document.getElementById("searchbar").value;
+    const url = `https://www.google.com/search?q=${encodeURIComponent(text)}`;
+    window.open(url, "_self");
+  }
 }
 
 function startTime() {
-    const currentDate = new Date();
-    document.getElementById("header-time").innerHTML = checkTime(currentDate.getHours()) + ":" + checkTime(currentDate.getMinutes());
+  const currentDate = new Date();
+  document.getElementById("header-time").innerHTML =
+    checkTime(currentDate.getHours()) +
+    ":" +
+    checkTime(currentDate.getMinutes());
 
-    const dateOptions = {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric"
-    };
+  const dateOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    weekday: "long",
+  };
 
-    document.getElementById("header-date").innerHTML = currentDate.toLocaleDateString("en-GB", dateOptions);
+  document.getElementById("header-date").innerHTML =
+    currentDate.toLocaleDateString("en-GB", dateOptions);
 
-    setTimeout(startTime, (60 - currentDate.getSeconds()) * 1000);
+  setTimeout(startTime, (60 - currentDate.getSeconds()) * 1000);
 }
 
 function checkTime(time) {
-    if (time < 10) {time = "0" + time};
-    return time;
+  if (time < 10) {
+    time = "0" + time;
+  }
+  return time;
 }
-document.getElementById("container").addEventListener('DOMContentLoaded', startTime());
+document
+  .getElementById("container")
+  .addEventListener("DOMContentLoaded", startTime());
